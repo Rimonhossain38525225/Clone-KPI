dea4bbedbf73db4ac1dc47c9e077d831d93f398b
Rimon hossain <rimonhossen333@gmail.com>
2023-10-13 00:47:41 +0600

    second commit

--- src/pages/home/banner_area/Banner.jsx
+++ src/pages/home/banner_area/Banner.jsx
@@ -1,15 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import "./banner.css";
+
+import myImage from "../../.././assets/banner_image/padmabanner.jpg";
 const Banner = () => {
   return (
     <>
       <div className="banner_area">
         <div className="banner">
-          <img
-            src="./src/assets/banner_image/padmabanner.jpg"
-            alt="banner image"
-          />
+          <img src={myImage} alt="banner image" />
         </div>
       </div>
     </>
